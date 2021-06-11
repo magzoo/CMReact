4a4cc74c6de50659e8a9e91ea20fe9989edd09ce
Duarte Conceição <190221077@estudantes.ips.pt>
2021-06-11 18:46:26 +0100

    sabado meter a funcionar o addevent e passagem de ecras

---
+++ screens/EventsScreen/AddEvent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  View,Text } from 'react-native';
+
+
+const AddEvent = () =>{
+    return(
+        <View>
+            <Text>AddEvent</Text>
+        </View>
+    )
+}
+
+export default AddEvent;
--- screens/EventsScreen/Buttons.js
+++ screens/EventsScreen/Buttons.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import {  View,TouchableOpacity, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import styles from './Styles'
+import AddingEvent from './AddEvent';
 
-const Buttons = () => {
+
+const AddEvent = () => {
+    return(
+        <AddingEvent/>
+    )
+}
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="AddEvent" component={AddEvent} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const Buttons = ({navigation}) => {
     return (
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonTitle}>Adicionar Evento</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonTitle}>Remover Evento</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonTitle}>Editar Evento</Text>
-            </TouchableOpacity>
-        </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddEvent')}>
+                    <Text style={styles.buttonTitle}>Adicionar Evento</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonTitle}>Remover Evento</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonTitle}>Editar Evento</Text>
+                </TouchableOpacity>
+            </View>
     );
 };
 
