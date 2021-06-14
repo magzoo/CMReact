def7b0696951b22fe269ada48859e6ca6b12e202
Duarte Conceição <190221077@estudantes.ips.pt>
2021-06-14 16:59:37 +0100

    Mapa Funciona

--- src/Components/AddEventComponents/EventMap.js
+++ src/Components/AddEventComponents/EventMap.js
@@ -1,33 +1,27 @@
 import React, {useState} from 'react';
 import {View, Button, Platform, Text} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import { exp } from 'react-native/Libraries/Animated/Easing';
 
-function getInitialState() {
-    return {
-      region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-    };
-  }
-  
- function onRegionChange(region) {
-    this.setState({ region });
-  }
+import styles from './Styles'
 
   const EventMap = () => {
       return(
-          <MapView
-            initialRegion={{
+        <MapView style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        >
+          <Marker coordinate={{
             latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-            }}
-            />
+            longitude: -122.4324,}}
+            image={require('../../../Assets/images/pin.png')}
+            style={styles.marker}
+            resizeMode="contain"></Marker>
+        </MapView>
       )
   }
 
---
+++ src/Components/AddEventComponents/Styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    map: {
+      height: 300,
+    },
+    marker: {
+        height: '10%',
+        width: '10%',
+    }
+});
+
+export default styles;
