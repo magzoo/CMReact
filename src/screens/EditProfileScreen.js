cb59ae87482be61c1defd35d1208d2a98cd571f0
JoseMendes1111 <62520673+JoseMendes1111@users.noreply.github.com>
2021-06-19 21:40:57 +0100

    update Editprofile
    dhfbdfuihd

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,27 +1,43 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React, {useState,useEffect,useContext}from 'react';
-import { Icon } from 'react-native-elements'
-import {View, StyleSheet,Text, Button,SafeAreaView,ScrollView,Image, TouchableOpacity,ImageBackground,TextInput} from 'react-native';
-import * as ImagePicker from "react-native-image-picker";
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React, {useState, useEffect, useContext} from 'react';
+import * as ImagePicker from 'react-native-image-picker';
 import auth from '@react-native-firebase/auth';
-import PasswordInputText from 'react-native-hide-show-password-input';
-export default class EditProfile extends React.Component{
-	state = {
-    resourcePath:null,
-    res:null,
-    email:null,
-    displayName :null,
-}
-  imageGalleryLaunch = () => {
+import firestore from '@react-native-firebase/firestore';
+import database from '@react-native-firebase/database';
+import {
+  Alert,
+  View,
+  StyleSheet,
+  Text,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+} from 'react-native';
+const EditProfileScreen = props => {
+  const [resourcePath, setResourcePath] = useState(null);
+  const [res, setRes] = useState(null);
+  const [uname, setUName] = useState('');
+  const [uemail, setUEmail] = useState('');
+  const [olddPassword, setOldPassword] = useState('');
+  const [newpassword, setNewPassword] = useState('');
+  const [userimage, setUserImage] = useState('');
+  const [user, setUser] = useState();
+  const {uid} = auth().currentUser.uid;
+
+  function imageGalleryLaunch() {
     let options = {
       storageOptions: {
         skipBackup: true,
         path: 'images',
       },
-    };   
-    ImagePicker.launchImageLibrary(options, (res) => {
+    };
+    ImagePicker.launchImageLibrary(options, res => {
       console.log('Response = ', res);
 
       if (res.didCancel) {
@@ -33,139 +49,189 @@ export default class EditProfile extends React.Component{
         alert(res.customButton);
       } else {
         console.log('response', JSON.stringify(res));
-        this.setState({resourcePath:res.assets[0].uri});
+        setResourcePath(res.assets[0].uri);
       }
     });
-  }  
-  render(){
-    const {email, displayName, password} = auth().currentUser;
-    this.state.email = email;
-    this.state.displayName = displayName;
-    const [userData, setUserData] = useState(null);
-
-  const getUser = async() => {
-    const currentUser = await firestore()
-    .collection('Users')
-    .doc(user.uid)
-    .get()
-    .then((documentSnapshot) => {
-      if( documentSnapshot.exists ) {
-        console.log('User Data', documentSnapshot.data());
-        setUserData(documentSnapshot.data());
-      }
-    })
   }
+  const getUser = async () => {
+    try {
+      const documentSnapshot = await firestore()
+        .collection('Users')
+        .doc()
+        .get();
+
+      const userData = documentSnapshot.data();
+      setUser(userData);
+    } catch {
+      //do whatever
+    }
+  };
+
+  // Get user on mount
   useEffect(() => {
     getUser();
   }, []);
 
-    console.log('this.state.password', password);
-      return(
-          <View style={styles.container}>
-              <View style={{margin:20}}>
-                <View style={{alignItems:'center'}}>
-                  <TouchableOpacity onPress={this.imageGalleryLaunch}>
-                 
-                    <View style={{height:100,width:100,borderRadius:15,
-                      justifyContent:'center',alignItems:'center'}}>
-                      <ImageBackground   
-                        source={require ('../Images/her.jpg',)}
-                        style={{height:100,width:100}}
-                        imageStyle={{borderRadius:50}}
-                      >
+  function handleUpdate() {
+    if (uname != '') {
+      firestore()
+        .collection('Users')
+        .where('email', '==', uemail)
+        .get()
+        .then(querySnapShot => {
+          querySnapShot.docs[0].ref.update({
+            name: uname,
+          });
+        });
+      Alert.alert('Perfil Atualizado Com Sucesso!');
+    }
+  }
 
-                     
-                      <ImageBackground   
-                        source={{uri:this.state.resourcePath}} 
-                        style={{height:100,width:100}}
-                        imageStyle={{borderRadius:50}}
-                      >
-
-                          <View>
-                          
-                          </View>
-                          <View>
-                              <Image source={require ('../Images/camera.png',)} size={35} style={{
-                                height:30,width:30,
-                                opacity:0.7,
-                                alignItems:'center',
-                                marginLeft:3,
-                                marginTop:12,
-
-                              }}/>
-                          </View>
-                      </ImageBackground>
-                      </ImageBackground>
-                    </View>
-                  </TouchableOpacity>
-                  <Text style={{marginTop:10,fontSize:18,fontWeight:'bold'}}>{this.state.displayName} </Text>
-                </View>
-                <View style={styles.action}>
-                <Image source={require ('../Images/nome.png',)} size={35} style={{
-                                height:20,width:20,
-                              }}/>
-                  <TextInput
-                      placeholder={this.state.displayName} 
-                      placeholderTextColor='#6666666'
-                      autoCorrect={false}
-                      style={styles.textInput}
-                  />
-
-                </View>
-                <View style={styles.action}>
-                  
-               
-                <Image source={require ('../Images/email.png',)} size={35} style={{
-                                height:20,width:20,
-                              }}/>
-                  <TextInput
-                      placeholder={this.state.email} 
-                      placeholderTextColor='#6666666'
-                      autoCorrect={false}
-                      style={styles.textInput}
-                  />
-
-                </View>
-                <View style={styles.action}>
-                <Image source={require ('../Images/pass.png',)} size={35} style={{
-                                height:20,width:20,
-                              }}/>
-                  <TextInput
-                      placeholder="Antiga Password"
-                      placeholderTextColor='#6666666'
-                      autoCorrect={false}
-                      style={styles.textInput}
-                  />
-                 
-                </View>
-                <View style={styles.action}>
-                <Image source={require ('../Images/pass.png',)} size={35} style={{
-                                height:20,width:20,
-                              }}/>
-                  <TextInput
-                      placeholder="Nova Password"
-                      placeholderTextColor='#6666666'
-                      autoCorrect={false}
-                      style={styles.textInput}
-                  />
-                 
-                </View>
-                <TouchableOpacity onPress={()=> {}}>
-                  <Button
-                      title="Atualizar"
-                      color="#669BF7"
-                      onPress={()=> {}}
-                  />
-                </TouchableOpacity>
-
-              </View>
-          </View>
-          
-    );
-  };
-  
-}
-
+  const {email, displayName, photoURL} = auth().currentUser;
+  var displayemail = email;
+  var displayname = displayName;
+  var imagesource = photoURL;
+  return (
+    <View style={styles.container}>
+      <View style={{margin: 20}}>
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity onPress={imageGalleryLaunch}>
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                borderRadius: 15,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <ImageBackground
+                source={{
+                  uri: resourcePath
+                    ? 'https://firebasestorage.googleapis.com/v0/b/cmtp2-342a4.appspot.com/o/her.jpg?alt=media&token=a164bc77-e4e9-427f-95c9-8b2704c8c937'
+                    : 'https://firebasestorage.googleapis.com/v0/b/cmtp2-342a4.appspot.com/o/her.jpg?alt=media&token=a164bc77-e4e9-427f-95c9-8b2704c8c937',
+                }}
+                style={{height: 100, width: 100}}
+                imageStyle={{borderRadius: 50}}>
+                <ImageBackground
+                  source={{uri: resourcePath}}
+                  style={{height: 100, width: 100}}
+                  imageStyle={{borderRadius: 50}}>
+                  <View></View>
+                  <View>
+                    <Image
+                      source={require('../Images/camera.png')}
+                      size={35}
+                      style={{
+                        height: 30,
+                        width: 30,
+                        opacity: 0.7,
+                        alignItems: 'center',
+                        marginLeft: 3,
+                        marginTop: 12,
+                      }}
+                    />
+                  </View>
+                </ImageBackground>
+              </ImageBackground>
+            </View>
+          </TouchableOpacity>
+          <Text style={{marginTop: 10, fontSize: 18, fontWeight: 'bold'}}>
+            {user && user?.name}{' '}
+          </Text>
+        </View>
+        <View style={styles.action}>
+          <Image
+            source={require('../Images/nome.png')}
+            size={35}
+            style={{
+              height: 20,
+              width: 20,
+            }}
+          />
+          <TextInput
+            placeholder={displayname}
+            placeholderTextColor="#6666666"
+            autoCorrect={false}
+            onChangeText={value => setUName(value)}
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.action}>
+          <Image
+            source={require('../Images/email.png')}
+            size={35}
+            style={{
+              height: 20,
+              width: 20,
+            }}
+          />
+          <TextInput
+            placeholder={displayemail}
+            placeholderTextColor="#6666666"
+            autoCorrect={false}
+            onChangeText={value => setUEmail(value)}
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.action}>
+          <Image
+            source={require('../Images/pass.png')}
+            size={35}
+            style={{
+              height: 20,
+              width: 20,
+            }}
+          />
+          <TextInput
+            placeholder="Antiga Password"
+            placeholderTextColor="#6666666"
+            autoCorrect={false}
+            onChangeText={value => setOldPassword(value)}
+            style={styles.textInput}
+          />
+        </View>
+        <View style={styles.action}>
+          <Image
+            source={require('../Images/pass.png')}
+            size={35}
+            style={{
+              height: 20,
+              width: 20,
+            }}
+          />
+          <TextInput
+            placeholder="Nova Password"
+            placeholderTextColor="#6666666"
+            autoCorrect={false}
+            onChangeText={value => setNewPassword(value)}
+            style={styles.textInput}
+          />
+        </View>
+        <TouchableOpacity>
+          <Button
+            title="Atualizar"
+            color="#669BF7"
+            onPress={() => {
+              if (uname != '') {
+                firestore()
+                  .collection('Users')
+                  .where('email', '==', uemail)
+                  .get()
+                  .then(querySnapShot => {
+                    querySnapShot.docs[0].ref.update({
+                      name: uname,
+                    });
+                  });
+                auth().currentUser.reload();
+                Alert.alert('Perfil Atualizado Com Sucesso!', uname);
+              }
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -249,4 +315,4 @@ const styles = StyleSheet.create({
   },
 });
 
-
+export default EditProfileScreen;
